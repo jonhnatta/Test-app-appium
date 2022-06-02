1845ebb856609cf56ae14507e1c5ee6405a15091
jonhnatta <jonatta89@gmail.com>
2022-06-02 11:52:12 -0300

    corrido problema ao rodar todos os códigos, foi preciso juntar em 1 arquivo apenas

--- test/specs/apiDemos.spec.js
+++ test/specs/apiDemos.spec.js
@@ -95,3 +95,92 @@ describe("testando Alert Dialogs", () => {
 
 })
 
+describe("testando Display Options", () => {
+
+  afterEach(async () => {
+    driver.reset()
+  })
+
+  it("verifica display home as up", async () => {
+    await $("//android.widget.TextView[@content-desc='App']").click()
+    await $("//android.widget.TextView[@content-desc='Action Bar']").click()
+    await $("//android.widget.TextView[@content-desc='Display Options']").click()
+    await $("//android.widget.Button[@content-desc='DISPLAY_HOME_AS_UP']").click()
+    const conteudo = await $("//android.widget.ImageButton[@content-desc='Navigate up']").isDisplayed()
+    expect(conteudo).toBe(true)
+  })
+
+  it("verifica display show title", async () => {
+    await $("//android.widget.TextView[@content-desc='App']").click()
+    await $("//android.widget.TextView[@content-desc='Action Bar']").click()
+    await $("//android.widget.TextView[@content-desc='Display Options']").click()
+    await $("//android.widget.Button[@content-desc='DISPLAY_SHOW_TITLE']").click()
+    const conteudo = await $('android=new UiSelector().text("App/Action Bar/Display Options").className("android.widget.TextView")').isDisplayed()
+    expect(conteudo).toBe(false)
+  })
+
+  it("verifica display show custom", async () => {
+    await $("//android.widget.TextView[@content-desc='App']").click()
+    await $("//android.widget.TextView[@content-desc='Action Bar']").click()
+    await $("//android.widget.TextView[@content-desc='Display Options']").click()
+    await $("//android.widget.Button[@content-desc='DISPLAY_SHOW_CUSTOM']").click()
+    const conteudo = await $('android=new UiSelector().text("CUSTOM VIEW!").className("android.widget.Button")').isDisplayed()
+    expect(conteudo).toBe(true)
+  })
+
+  it("verifica display show navigation", async () => {
+    await $("//android.widget.TextView[@content-desc='App']").click()
+    await $("//android.widget.TextView[@content-desc='Action Bar']").click()
+    await $("//android.widget.TextView[@content-desc='Display Options']").click()
+    await $("//android.widget.Button[@content-desc='Navigation']").click()
+    const tab1 = await $('android=new UiSelector().text("TAB 1").className("android.widget.TextView")').isDisplayed()
+    expect(tab1).toBe(true)
+    const tab2 = await $('android=new UiSelector().text("TAB 2").className("android.widget.TextView")').isDisplayed()
+    expect(tab2).toBe(true)
+    const tab3 = await $('android=new UiSelector().text("TAB 3").className("android.widget.TextView")').isDisplayed()
+    expect(tab3).toBe(true)
+  })
+
+
+})
+
+describe("testando fragment hide and show", () => {
+
+  afterEach(async () => {
+    driver.reset()
+  })
+
+  it("verifica conteudo de OK CANCEL DIALOG WITH A MESSAGE", async () => {
+    await $("//android.widget.TextView[@content-desc='App']").click()
+    await $("//android.widget.TextView[@content-desc='Fragment']").click()
+    await $("//android.widget.TextView[@content-desc='Hide and Show']").click()
+    const conteudo = await $('android=new UiSelector().text("Demonstration of hiding and showing fragments.").className("android.widget.TextView")').isDisplayed()
+    expect(conteudo).toBe(true)
+  })
+
+  it("verifica primeiro botão hide", async () => {
+    await $("//android.widget.TextView[@content-desc='App']").click()
+    await $("//android.widget.TextView[@content-desc='Fragment']").click()
+    await $("//android.widget.TextView[@content-desc='Hide and Show']").click()
+    await $("id=io.appium.android.apis:id/frag1hide").click()
+    const botao = await $('android=new UiSelector().text("SHOW").className("android.widget.Button")').getText()
+    expect(botao).toBe("SHOW")
+    const text = await $("id=io.appium.android.apis:id/fragment1").isDisplayed()
+    expect(text).toBe(false)
+
+  })
+
+  it("verifica segundo botão hide", async () => {
+    await $("//android.widget.TextView[@content-desc='App']").click()
+    await $("//android.widget.TextView[@content-desc='Fragment']").click()
+    await $("//android.widget.TextView[@content-desc='Hide and Show']").click()
+    await $("id=io.appium.android.apis:id/frag2hide").click()
+    const botao = await $('android=new UiSelector().text("SHOW").className("android.widget.Button")').getText()
+    expect(botao).toBe("SHOW")
+    const text = await $("id=io.appium.android.apis:id/fragment2").isDisplayed()
+    expect(text).toBe(false)
+  })
+
+})
+
+
